bea1bcd1c90662b8237da07abfdda13f3290e10c
Luciana <lt.shimoyama@gmail.com>
2024-07-25 08:51:14 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		// para rodar o programa colorido, abrir o git bash na pasta bin do projeto e no terminal digitar:  java application/Program
+		
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
